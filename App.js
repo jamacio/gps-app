fbbd6aa8c608674657beb453bc1df2441b106d0f
Jamacio <jamacio.rocha@danone.com>
2023-08-30 14:28:43 -0300

    update

--- App.js
+++ App.js
@@ -1,11 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Location from 'expo-location';
+import * as Battery from 'expo-battery';
+import * as TaskManager from 'expo-task-manager';
+
+const LOCATION_TASK_NAME = 'background-location-task';
+const TIME_TO_UPDATE = 1000;
 
 export default function App() {
+  const [deviceData, setDeviceData] = useState({});
+  const [teste, setTeste] = useState(0);
+
+  const updateLatLong = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    const prositionInfo = await Location.getCurrentPositionAsync({});
+    const batteryInfo = await Battery.getPowerStateAsync();
+    console.log(batteryInfo);
+    setDeviceData({ ...prositionInfo, ...batteryInfo });
+  }
+
+  const requestPermissions = async () => {
+    const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
+
+    if (foregroundStatus === 'granted') {
+      const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
+
+      if (backgroundStatus === 'granted') {
+        await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+          accuracy: Location.Accuracy.Balanced,
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+
+      updateLatLong();
+    }, TIME_TO_UPDATE);
+  })
+
+  TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+    if (error) {
+      return;
+    }
+
+    if (data) {
+      let testetetse = teste;
+      setInterval(() => {
+        setTeste(testetetse++)
+        updateLatLong();
+      }, TIME_TO_UPDATE);
+    }
+  });
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>lat: {deviceData?.coords?.latitude} </Text>
+      <Text>long: {deviceData?.coords?.longitude} </Text>
+      <Text>------------------</Text>
+      <Text>batteryLevel: {deviceData?.batteryLevel} </Text>
+      <Text>batteryState: {deviceData?.batteryState} </Text>
+      <Text>lowPowerMode: {deviceData?.lowPowerMode ? 'true' : 'false'} </Text>
+      <Text>teste: {teste} </Text>
+      <Button onPress={requestPermissions} title="Enable background location" />
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+yarn
+yarn start
+
+npm install -g eas-cli
+eas build -p android
+
+eas build -p android --profile preview-apk
